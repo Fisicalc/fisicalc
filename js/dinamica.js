652fdf5812e1ab109a35eee0442e61cd37f99ccd
Lemberen <55971095+Lemberen@users.noreply.github.com>
2019-12-16 21:10:11 -0300

    Add files via upload

--- js/dinamica.js
+++ js/dinamica.js
@@ -1,81 +1,143 @@
 'use strict'
 
-function energiaCinetica(massa, velocidade){
-    return massa / 2 * velocidade ** 2;
-}
+const dinamica = {
+    energiaCinetica:{
+        formula: "E_C=\\frac{1}{2}mv^2",
 
-function energiaMecanica(energiaCinetica, energiaPotencial){
-    return energiaCinetica + energiaPotencial;
-}
+        metodo: function(massa, velocidade){
+            return massa / 2 * velocidade ** 2;
+        }
+    },
 
-function energiaPotencialElastica(constanteElasticaDaMola, elongacaoDaMola){
-    return constanteElasticaDaMola / 2 * elongacaoDaMola ** 2;
-}
+    energiaMecanica:{
+        formula: "E_M=E_C+E_P",
+        
+        function(energiaCinetica, energiaPotencial){
+            return energiaCinetica + energiaPotencial;
+        }
+    },
 
-function energiaPotencialGravitacional(massa, gravidade, altura){
-    return massa * gravidade * altura;
-}
+    energiaPotencialElastica:{
+        formula: "E_{PE}=\\frac{1}{2}kx^2",
+        
+        metodo: function(constanteElasticaDaMola, elongacaoDaMola){
+            return constanteElasticaDaMola / 2 * elongacaoDaMola ** 2;
+        }
+    },
 
-function forcadeAtritoDinamico(coeficienteDeAtritoDinamico, forcaNormal){
-    return coeficienteDeAtritoDinamico * forcaNormal;
-}
+    energiaPotencialGravitacional:{
+        formula: "E_{PG}=mgh",
 
-function forcadeAtritoEstatico(coeficienteDeAtritoEstático, forcaNormal){
-    return coeficienteDeAtritoEstático * forcaNormal;
-}
+        metodo: function(massa, gravidade, altura){
+            return massa * gravidade * altura;
+        }
+    },
 
-function forcaCentripetaUm(massa, aceleracaoCentripeta){
-    return massa * aceleracaoCentripeta;
-}
+    forcadeAtritoDinamico:{
+        formula: "F_{AT}=\\mu_{din}N",
+        
+        metodo: function(coeficienteDeAtritoDinamico, forcaNormal){
+            return coeficienteDeAtritoDinamico * forcaNormal;
+        }
+    },
 
-function forcaCentripetaDois(massa, velocidade, raioDoCirculo){
-    return massa * velocidade ** 2 / raioDoCirculo;
-}
+    forcadeAtritoEstatico:{
+        formula: "F_{AT}=\\mu_{est}N",
+        
+        metodo: function(coeficienteDeAtritoEstático, forcaNormal){
+            return coeficienteDeAtritoEstático * forcaNormal;
+        }
+    },
 
-function forcaResultante(forca){
-    //Fazer depois    
-}
+    forcaCentripeta:{
+        formula1: "F_{CP}=ma_{CP}",
+        formula2: "F_{CP}=m\\frac{v^2}{r}",
+        
+        metodo1: function(massa, aceleracaoCentripeta){
+        return massa * aceleracaoCentripeta;
+        },
+        metodo2: function(massa, velocidade, raioDoCirculo){
+            return massa * velocidade ** 2 / raioDoCirculo;
+        }
+    },
 
-function impulso(forcaResultante, intervaloDeTempo){
-    return forcaResultante * intervaloDeTempo;
-}
+    forcaResultante:{
+        //Fazer depois
+    },
 
-function leiDeHooke(constanteElasticaDaMola, elongacaoDaMola){
-    return constanteElasticaDaMola * elongacaoDaMola;
-}
+    impulso:{
+        formula: "\\vec{I}=\\vec{F}\\Delta{t}",
+        
+        metodo: function(forcaResultante, intervaloDeTempo){
+            return forcaResultante * intervaloDeTempo;
+        }
+    },
 
-function pesoDeUmCorpo(massa, gravidade){
-    return massa * gravidade;
-}
+    leiDeHooke:{
+        formula: "F=kx",
 
-function potenciaInstantaneaUm(){
-    //Fazer depois
-}
+        metodo: function(constanteElasticaDaMola, elongacaoDaMola){
+            return constanteElasticaDaMola * elongacaoDaMola;
+        }
+    },
 
-function potenciaInstantaneaDois(forcaEscalarResultante, velocidade){
-    return forcaEscalarResultante * velocidade;
-}
+    pesoDeUmCorpo:{
+        formula: "\\vec{P}=m\\vec{g}",
+        
+        metodo: function(massa, gravidade){
+            return massa * gravidade;
+        }
+    },
 
-function potenciaMediaUm(trabalho, intervaloDeTempo){
-    return trabalho / intervaloDeTempo;
-}
+    potenciaInstantanea:{
+        formula1: "P=\\lim_{t\\to0}\\vec{P}",
+        formula2: "P=F\\vec{v}",
 
-function potenciaMediaDois(forcaEscalarResultante, deslocamento, intervaloDeTempo){
-    return forcaEscalarResultante * deslocamento / intervaloDeTempo;
-}
+        metodo1: function(){
+        //Fazer depois
+        },
+        metodo2: function(forcaEscalarResultante, velocidade){
+            return forcaEscalarResultante * velocidade;
+        }
+    },
 
-function potenciaMediaTres(forcaEscalarResultante, velocidadeMedia){
-    return forcaEscalarResultante * velocidadeMedia;
-}
+    potenciaMedia:{
+        formula1: "\\vec{P}=\\frac{\\tau}{\\Delta{t}}",
+        formula2: "\\vec{P}=\\frac{Fd}{\\Delta{t}}",
+        formula3: "\\vec{P}=F\\vec{v}",
+        
+        metodo1: function(trabalho, intervaloDeTempo){
+            return trabalho / intervaloDeTempo;
+        },
+        metodo2: function(forcaEscalarResultante, deslocamento, intervaloDeTempo){
+            return forcaEscalarResultante * deslocamento / intervaloDeTempo;
+        },
+        metodo3:function(forcaEscalarResultante, velocidadeMedia){
+            return forcaEscalarResultante * velocidadeMedia;
+        }
+    },
 
-function quantidadeDeMovimento(massa, velocidade){
-    return massa * velocidade;
-}
+    quantidadeDeMovimento:{
+        formula: "\\vec{Q}=m\\vec{v}",
 
-function segundaLeiDeNewton(massa, aceleracaoEscalar){
-    return massa * aceleracaoEscalar;
-}
+        metodo: function(massa, velocidade){
+            return massa * velocidade;
+        }
+    },
 
-function trabalho(forcaEscalarResultante, deslocamento, cosAnguloFormadoComAHorizontal){
-    return forcaEscalarResultante  * deslocamento * cosAnguloFormadoComAHorizontal;
+    segundaLeiDeNewton:{
+        formula: "F=ma",
+        
+        metodo: function(massa, aceleracaoEscalar){
+            return massa * aceleracaoEscalar;
+        }
+    },
+
+    trabalho:{
+        formula: "\\tau=F\\cdot{d}\\cdot{cos\\theta}",
+
+        metodo: function(forcaEscalarResultante, deslocamento, cosAnguloFormadoComAHorizontal){
+            return forcaEscalarResultante  * deslocamento * cosAnguloFormadoComAHorizontal;
+        }
+    },
 }
